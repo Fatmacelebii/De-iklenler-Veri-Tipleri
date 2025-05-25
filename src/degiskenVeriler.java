c61929d7f3c90580ba683de14cac86a3411381b2
Fatma çelebi <fatmakaygisiz97@gmail.com>
2025-05-25 21:52:34 +0300

    veri

---
+++ src/degiskenVeriler.java
@@ -0,0 +1,22 @@
+public class degiskenVeriler {
+public static void main(String[] args) {
+    int a = 100001;
+    System.out.println(a);
+    float ab = 25;
+    float abc = 26f;
+    float abcd = 27F;
+    System.out.println(ab);
+
+    double dbl = 102.5;
+    System.out.println(dbl);
+
+    char karakter = 126;
+    System.out.println(karakter);
+
+    boolean mantık = true;
+    System.out.println(mantık);
+
+    String s = "final";
+    System.out.println(s);
+}
+}
